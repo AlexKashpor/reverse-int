f90a8fc2068316d4a762eb7133ac19e2773091ec
Alex <alex-88_90@mail.ru>
2021-03-15 19:30:20 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports =  function reverse (n) {
+    if(n<0){
+        return '-' + (Number(String(Math.abs(-n)).split('').reverse().join('')));
+    }
+    else {
+        return (Number(String(n).split('').reverse().join('')));
+    }
+    
 }
